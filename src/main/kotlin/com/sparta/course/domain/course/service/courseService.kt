91b45f206765593c182f05ed636f166d269ed30f
seunghwan <lovelyunsh@naver.com>
2024-02-06 12:45:58 +0900

    #1 refactor : service interface에 주석 추가

--- src/main/kotlin/com/sparta/course/domain/course/service/courseService.kt
+++ src/main/kotlin/com/sparta/course/domain/course/service/courseService.kt
@@ -14,43 +14,59 @@ import org.springframework.data.domain.Pageable
 
 interface CourseService {
 
+    // title을 contain하는 course 데이터 반환
     fun searchCourseList(title: String): List<CourseResponse>
 
+    // status 값이 일치하는 course 데이터에 paging 한 list를 반환
     fun getPaginatedCourseList(pageable: Pageable, status: String?): Page<CourseResponse>
 
+    // 전체 course 데이터 list 반환
     fun getAllCourseList(): List<CourseResponse>
 
+    // courseId 값이 일치하는 데이터 반환
     fun getCourseById(courseId: Long): CourseResponse
 
+    // CreateCourseRequest 값을 insert
     fun createCourse(request: CreateCourseRequest): CourseResponse
 
+    // courseId 값이 일치하는 데이터를 UpdateCourseRequest로 update
     fun updateCourse(courseId: Long, request: UpdateCourseRequest): CourseResponse
 
+    // courseId 값이 일치하는 데이터를 삭제
     fun deleteCourse(courseId: Long)
 
+    // courseId 값이 일치하는 데이터의 Lecture 데이터 반환
     fun getLectureList(courseId: Long): List<LectureResponse>
 
+    // courseId 값이 일치하는 데이터의 Lecture 정보 중 lectureId 값이 일치하는 데이터 반환
     fun getLecture(courseId: Long, lectureId: Long): LectureResponse
 
+    // courseId와 AddLectureRequest 값을 이용해 Lecture에 insert
     fun addLecture(courseId: Long, request: AddLectureRequest): LectureResponse
 
+    // courseId와 lectureId가 일치하는 Lecture 데이터를 UpdateLectureRequest 데이터로 수정
     fun updateLecture(
         courseId: Long,
         lectureId: Long,
         request: UpdateLectureRequest
     ): LectureResponse
 
+    // courseId와 lectureId가 일치하는 Lecture 데이터를 삭제
     fun removeLecture(courseId: Long, lectureId: Long)
 
+    // courseId와 ApplyCourseRequest 값을 이용해 CourseApplication에 insert
     fun applyCourse(courseId: Long, request: ApplyCourseRequest): CourseApplicationResponse
 
+    // courseId와 applicationId 일치하는 CourseApplication 데이터를 반환
     fun getCourseApplication(
         courseId: Long,
         applicationId: Long
     ): CourseApplicationResponse
 
+    // courseId가 일치하는 CourseApplication 데이터 list 반환
     fun getCourseApplicationList(courseId: Long): List<CourseApplicationResponse>
 
+    // courseId와 applicationId가 일치하는 CourseApplication 데이터의 상태를 UpdateApplicationStatusRequest 값으로 수정
     fun updateCourseApplicationStatus(
         courseId: Long,
         applicationId: Long,
--- src/main/kotlin/com/sparta/course/domain/user/service/UserService.kt
+++ src/main/kotlin/com/sparta/course/domain/user/service/UserService.kt
@@ -4,9 +4,12 @@ import com.sparta.course.domain.user.dto.*
 
 interface UserService {
 
+    // SignUpRequest 값을 이용해 회원가입
     fun signUp(signUpRequest: SignUpRequest): UserResponse
 
+    // userId 값이 일치하는 User 데이터를 UpdateUserProfileRequest 값으로 수정
     fun updateUserProfile(userId: Long, updateUserProfileRequest: UpdateUserProfileRequest): UserResponse
 
+    // LoginRequest 값을 비교하여 로그인 수행
     fun login(request: LoginRequest): LoginResponse
 }
